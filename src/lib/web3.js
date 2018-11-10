e57408ae396d0cdfc34a882bc5ad2f6af495818f
Jeremiah Goyette <jsgoyette@protonmail.com>
2018-11-10 09:04:40 +0000

    Add test for web3 watchLogs util func

--- src/lib/web3.js
+++ src/lib/web3.js
@@ -1,6 +1,7 @@
 const util = require('util');
 
 const ERR_NOVERSION = 'Unable to obtain web3 version';
+const TIMEOUT_INTERVAL = 3000
 
 module.exports = web3Util;
 
@@ -8,6 +9,12 @@ function web3Util(web3Obj) {
 
   this.web3 = web3Obj;
   this.watchLogs = watchLogs;
+  this.getVersion = getVersion;
+
+  // timeout method added so that tests can bypass setTimeout
+  this.timeout = cb => {
+    setTimeout(() => cb(), TIMEOUT_INTERVAL);
+  }
 
   return this;
 }
@@ -29,16 +36,16 @@ function getVersion(web3) {
 
 function watchLogs(opts) {
   return new Promise((resolve, reject) => {
+
     const getLogs = () => {
       this.web3.eth.getPastLogs(opts).then(res => {
         if (res.length) {
           return resolve(res);
         }
 
-        // TODO: add some sort of timeout
-        setTimeout(() => {
-          getLogs();
-        }, 3000);
+        // TODO: add some sort of retry max
+        this.timeout(() => getLogs());
+
       }).catch(err => {
         reject(err);
       });
